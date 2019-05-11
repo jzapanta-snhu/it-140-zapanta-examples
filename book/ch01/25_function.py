64e1eb97a3c5ec5b4b165b232bde4ba815a2d2cf
Javier E. Zapanta <46081745+jzapanta-snhu@users.noreply.github.com>
2019-05-10 22:39:50 -0400

    Update 25_function.py
    Added more comments about use of sep=""

--- book/ch01/25_function.py
+++ book/ch01/25_function.py
@@ -2,7 +2,7 @@
 # DATE:		09 May 2019
 # COURSE: 	IT-140
 # PROGRAM: 	Chapter 1 - Function Example
-# PURPOSE:  This program will demonstrate how a custom-made function works.
+# PURPOSE:  	This program will demonstrate how a custom-made function works.
 # RUNTIME:	Python 3+
 
 def applyDiscount(price, discountRate):
@@ -18,7 +18,7 @@ def applyDiscount(price, discountRate):
 		float:  Calculated price with discount in effect.
 	"""
 
-	# calculate sale price and return result to original function call (Line 31)
+	# calculate sale price and return result to original function call (Line 33)
 	return price - (price * discountRate)
 
 # initialize salePrice and discount
@@ -26,6 +26,8 @@ salePrice = 29.99						# price
 discount = 0.10							# percentage as decimal
 
 print ("SALE PRICE CALCULATION\n")
+# Use of sep="" gets rid of the default separator when using a comma in a print statement to
+# combine an output stirng.
 print ("Price: $", salePrice, sep="")
 print ("DiscountRate:  ", (discount * 100), "%", sep="")
 print ("Discounted price:  $", applyDiscount(salePrice, discount), sep="")
